0fbf61e741bd857b17d48202654d93293ad6dff7
Nerensio <nicolas.dumont84850@gmail.com>
2020-09-18 15:49:37 +0200

    first commit

---
+++ Assets/TamagoshiScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tamagoshi : MonoBehaviour
+{
+
+    private Tamagoshi _tamagoshi : new Tamagoshi(10, 10, 10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
